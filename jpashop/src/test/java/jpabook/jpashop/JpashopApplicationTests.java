4faffcf9f0eec25387eaa88cfa745dc6dd322641
yunbo3 <yunbo3@naver.com>
2023-12-25 01:01:56 +0900

    초기세팅

--- jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class JpashopApplicationTests {
+public class JpashopApplicationTests {
 
 	@Test
 	void contextLoads() {
